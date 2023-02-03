64fc9c20a30c1c7119b8a03abbb900ff68e74b22
rafaelbocanegra93 <rafaboca93@gmail.com>
2023-02-03 02:47:37 -0500

    replace close button selector

--- tests/wishlist.spec.ts
+++ tests/wishlist.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('wishlist', async ({ page }) => {
   await page.goto('https://www.adidas.co/');
   await page.locator('a.teaser-card-wrapper___1FO3l').nth(0).click();
-  await page.getByRole('button', { name: 'Cerrar' }).click();
+  await page.locator('button.gl-modal__close').click();
   await page.locator('.glass-product-card__wishlist > div > .gl-icon__wrapper').first().click();
   await page.locator('input.gl-input__field').fill('rafaboca93@gmail.com');
   await page.locator('button[type="submit"]').click();
