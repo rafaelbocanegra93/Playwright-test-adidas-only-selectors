c711825ac0ab4a6da1a6e3f6c0d28f8525ceb088
rafaelbocanegra93 <rafaboca93@gmail.com>
2023-02-02 23:46:17 -0500

    Update selectors

--- tests/wishlist.spec.ts
+++ tests/wishlist.spec.ts
@@ -1,15 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('wishlist', async ({ page }) => {
-  await page.goto('https://www.adidas.co/');;
-  await page.getByRole('link', { name: 'Content Card With Link' }).first().click();
+  await page.goto('https://www.adidas.co/');
+  await page.locator('a.teaser-card-wrapper___1FO3l').nth(0).click();
   await page.getByRole('button', { name: 'Cerrar' }).click();
-  await page.getByTestId('wishlist-inactive').click();
-  await page.getByPlaceholder(' ').click();
-  await page.getByPlaceholder(' ').fill('rafaboca93@gmail.com');
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await page.locator('.glass-product-card__wishlist > div > .gl-icon__wrapper').first().click();
+  await page.locator('input.gl-input__field').fill('rafaboca93@gmail.com');
+  await page.locator('button[type="submit"]').click();
+  await page.locator('input[type="password"]').fill('rafa1207@');
+  await page.locator('button[type="submit"]').click();
 
 });
